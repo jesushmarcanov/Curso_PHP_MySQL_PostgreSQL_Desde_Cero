edbdcad0364ee128f56f4f273ba331f396f17a36
Chewiie <jesushmarcanov@gmail.com>
2020-10-27 17:11:54 -0400

    operadores en PHP

---
+++ 12_operadores/operadores.php
@@ -0,0 +1,131 @@
+<?php
+    /* Operadores Aritméticos:
+
+    +  Suma
+    -  Resta
+    *  Multiplicación
+    /  División
+    %  Módulo o Resto
+        
+    */
+    // $numero = 10;
+    // $numero2 = 5;
+    // $numero3 = 6;
+
+    // $suma = $numero + $numero2;
+    // $resta = $numero - $numero2;
+    // $multiplicacion = $numero * $numero2;
+    // $division = $numero / $numero2;
+    // $modulo = $numero % $numero3;
+
+
+    // echo 'La Suma es: ', $suma . "\n"; //Salto de linea en php
+    // echo 'La Resta es: ', $resta. "\n";
+    // echo 'La Multiplicación es: ', $multiplicacion. "\n";
+    // echo 'La Division es: ', $division. "\n";
+    // echo 'El Residuo de la división es: ', $modulo. "\n";
+
+
+    /* Operadores de Asignación:
+     
+    =
+    +=
+    -=
+    *=
+    /=
+    
+    */
+
+    // $numero = 10;
+    // $numero2 = 5;
+    // $numero3 = 6;
+
+    // //$numero = $numero + 7; //acumulador: toma e valor de una variable y la suma por otro valor
+    // $numero += 7;
+    // echo $numero;
+
+
+    /* Operadores de Comparación:
+     
+    == igual que
+    === identico a
+    !=, <> diferente de
+    !== no sea identico a
+    > mayor que
+    < menor que
+    >= mayor o igual
+    <= menor o igual
+
+    */
+
+    // $numero = '10';
+    // $variable = 'true';
+
+    // if ($numero === 10){
+    //     echo 'Es identico';
+    // } else {
+    //     echo 'Es diferente';
+    // }
+
+    // if ($variable === true){
+    //     echo 'Es identico';
+    // } else {
+    //     echo 'Es diferente';
+    // }
+
+    /* Operadores Lógicos:
+     
+    and, &&
+    or, ||
+    xor
+    !
+
+    */
+    // $numero = 10;
+    // if ($numero >= 10 && $numero < 20){
+    //     echo 'Ejecuta';
+    // }
+
+    /* Operadores de Incremento/Decremento: Contadores
+     
+    ++$x
+    $x++
+    --$x
+    $x--
+
+    */
+    //$numero = 10;
+    // $numero2 = 10;
+    // $numero3 = 10;
+    // $numero4 = 10;
+
+    // $numero = $numero + 1;
+    // $numero2 += 1;
+    // $numero3++;
+    
+
+    // echo $numero . "\n";
+    // echo $numero2 . "\n";
+    // echo $numero3 . "\n";
+    // echo $numero4++ . "\n";
+    // echo $numero--;
+    // echo "\n";
+    // echo $numero;
+
+
+    /* Operadores de Cadenas:
+     
+    .
+    .=
+
+    */
+
+    // $texto = 'Una Cadena';
+    // $texto2 = ' Otra Cadena';
+    // $cadena = $texto . $texto2;
+    // echo $cadena;
+
+    $texto = 'Había una vez';
+    $texto .= 'Un gatico muy bonito'; 
+
+?>

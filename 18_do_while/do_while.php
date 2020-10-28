c11cf5c1d6a745ecac122f7777470bbfd24878a4
Chewiie <jesushmarcanov@gmail.com>
2020-10-28 17:46:16 -0400

    Ciclo do while

---
+++ 18_do_while/do_while.php
@@ -0,0 +1,14 @@
+<?php
+    //Ciclo Do While: con este ciclo se ejecutará la instrucción al menos 1 vez mientras haya una condicion determinada.
+    /*
+    Sintaxis:
+    do{
+        instrucciones
+    } while(condiciones)
+    
+    */
+    $i = 1;
+    do {
+        echo $i . "\n";
+        $i++;
+    } while($i <= 20);

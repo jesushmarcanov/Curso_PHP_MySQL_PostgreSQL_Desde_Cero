ed798263d108501a7496a1acbda2c518f8ca66c0
Chewiie <jesushmarcanov@gmail.com>
2020-10-29 18:15:03 -0400

    Funciones en PHP

---
+++ 23_funciones/funciones.php
@@ -0,0 +1,25 @@
+<?php
+    //Funciones en PHP
+    /*
+        Una función es un bloque de código que podemos programar para ejecutar cuantas veces querramos y donde nosotros querramos con una sola linea de código
+        Sintaxis:
+        function nombre(parametros){
+            Instrucciones
+        }
+
+        Algunas de las funciones predefinidas de php son:
+        // count();
+        // sort();
+        // rsort();
+        // var_dump();
+        // print_r();
+        // gettype();
+        // getdate();
+    */
+
+    function saludo(){
+        echo "Doy un saludo";
+    }
+
+    saludo();
+    saludo();

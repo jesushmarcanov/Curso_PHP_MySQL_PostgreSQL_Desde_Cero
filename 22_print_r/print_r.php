9a776ab4b0ddf8ec1ec2cfead6d96a12e904f22b
Chewiie <jesushmarcanov@gmail.com>
2020-10-29 17:53:22 -0400

    Funcion print_r

---
+++ 22_print_r/print_r.php
@@ -0,0 +1,11 @@
+<?php
+    //Funcion print_r(): Función que nos muestra información de forma legible para conocer de una forma rápida los valores de las variables.
+
+$nombre = 'Jesús';
+$numero = 10;
+$numero2 = '5';
+$arreglo = array('Carlos','Cesar','Alejandro');
+$arreglo_asociativo = array('nombre' => 'Carlos', 'edad' => 20);
+$booleano = true;
+
+print_r($booleano);

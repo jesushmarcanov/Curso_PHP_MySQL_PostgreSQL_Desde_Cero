58ae8d61a918b373c2332786f4e58b14f07ffddd
Chewiie <jesushmarcanov@gmail.com>
2020-10-29 18:55:53 -0400

    Funciones con parámetros

---
+++ 24_parametros/funciones2.php
@@ -0,0 +1,21 @@
+<?php
+    // function saludo($nombre){
+    //     echo 'Hola, ' . $nombre;
+    //     echo "\n";
+    // }
+
+    // saludo('Jesús');
+    // saludo('Juan');
+    // saludo('José');
+
+    function sumar($num1, $num2){
+        $resultado = $num1 + $num2;
+        echo 'El resultado de la suma es: ' . $resultado;
+    }
+
+    sumar(10,15);
+    echo "\n";
+    sumar(3,4);
+    echo "\n";
+    sumar(2,28);
+    echo "\n";

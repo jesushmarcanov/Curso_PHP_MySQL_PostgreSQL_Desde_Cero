6cd57fbe46032d0cb88e2779a0fcbffa0656062b
Chewiie <jesushmarcanov@gmail.com>
2020-10-30 19:00:23 -0400

    Funciones Matematicas

---
+++ 29_maths/f_maths.php
@@ -0,0 +1,14 @@
+<?php
+    //Funciones Matemáticas en PHP
+
+    //round(): Nos permite redondear un numero decimal. Si le pasamos como segundo parámetro un numero, esta nos redondea solo los decimales en vez del número completo
+    $numero = 15.785;
+    //echo round($numero, 1);
+
+    //rand(): De random, nos permite mostrar un número aleatorio o al azar. Se le pasan dos parámetros: número inicial y número final de la serie numérica a evaluar.
+    //echo rand(1,10);
+
+    //ceil(): Funcion que nos permite redondear un número hacia arriba
+    //echo ceil(12.1);
+
+    echo M_PI; //Constante de pi definida por PHP

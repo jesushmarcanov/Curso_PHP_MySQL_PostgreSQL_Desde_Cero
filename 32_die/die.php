a92239f0f90144c8e0939d9c980d4135d271c401
Chewiie <jesushmarcanov@gmail.com>
2020-10-30 20:06:19 -0400

    Funcion die

---
+++ 32_die/die.php
@@ -0,0 +1,19 @@
+<?php
+    //Funcion die(): Esta funcion nos va a permitir parar la ejecución de la página por completo.
+    echo "Soy un echo";
+    // $numero = 10;
+    // var_dump($numero);
+    die();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hola Mundo</h1>
+</body>
+</html>

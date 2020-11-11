183e8ddf545d8f8d4d2f1f8b5b3169857d583a7d
Chewiie <jesushmarcanov@gmail.com>
2020-11-11 15:33:42 -0400

    Metodos GET y POST

--- 33_formularios/recibe.php
+++ 33_formularios/recibe.php
@@ -4,20 +4,40 @@
 
     //Ejemplo Sencillo
     //Aquí pregunta si no hay $_POST, es decir, si el arreglo $_POST está vacío, redirige el proceso al formulario.
-    if(!$_POST){
-        header('Location: localhost:8000');
-    }
+    // if(!$_POST){
+    //     header('Location: localhost:8000');
+    // }
         
-        $nombre = $_POST["nombre"];
-        $sexo = $_POST["sexo"];
-        $year = $_POST["year"];
-        $terminos = $_POST["terminos"];
+    //     $nombre = $_POST["nombre"];
+    //     $sexo = $_POST["sexo"];
+    //     $year = $_POST["year"];
+    //     $terminos = $_POST["terminos"];
 
-        echo 'Hola, ' . $nombre . ' eres ' .$sexo;
+    //     echo 'Hola, ' . $nombre . ' eres ' .$sexo;
 
 /*
     POST nunca enviará los valores por la URL, los enviará por detrás del navegador por así decirlo; con este método se peuden enviar archivos, y se utilizará para mostrar información importante que el usuario no pueda acceder.
     GET no envía arhivos, los valores solo los envia por la URL. Este método se utiliza ms que todo para tomar los valores o parámetros de la URL y mostrará información dependiendo de los parámetros, por ejemplo.
 */
 
+//var_dump($_GET);
+
+        if(!$_GET){
+            header('Location: localhost:8000');
+        }  
+
+        $nombre = $_GET["nombre"];
+        $sexo = $_GET["sexo"];
+        $year = $_GET["year"];
+        $terminos = $_GET["terminos"];
+
+        if($nombre){
+            echo $nombre . '<br />';
+        } else {
+            echo 'El usuario no estableció su nombre <br>';
+        }
     
+        
+        echo $sexo . '<br />';
+        echo $year . '<br />';
+        echo $terminos . '<br />';
